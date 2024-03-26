35f25870834826494d31ba972578e1cd5ce6b085
Ruan Blotta <ruanblotta@gmail.com>
2024-03-26 16:35:49 -0300

    Update: Resoltando a atividade com Orientação a Objeto

--- CalculoRetangulo/Program.cs
+++ CalculoRetangulo/Program.cs
@@ -6,27 +6,17 @@ namespace CalculoRetangulo
     {
         static void Main(string[] args)
         {
-            
+            Retangulo retangulo = new Retangulo();
+
             Console.WriteLine("Digite a altura do retângulo: ");
-            double altura = double.Parse(Console.ReadLine());
+            retangulo.Altura = double.Parse(Console.ReadLine());
 
             Console.WriteLine("Digite o largura do retângulo: ");
-            double largura = double.Parse(Console.ReadLine());
+            retangulo.Largura = double.Parse(Console.ReadLine());
 
-            // Area A = b * h
-
-            double area = altura * largura;
-            Console.WriteLine($"A área do retângulo é: {area.ToString("F2")} ");
-
-            // Perímmetro P = 2 (b+h)
-
-            double perimetro = 2 * (altura + largura);
-            Console.WriteLine("O perímetro é: " + perimetro.ToString("F2"));
-
-            // Diagonal a² = b² + c²
-
-            double diagonal = Math.Sqrt((altura * altura ) + (largura * largura));
-            Console.WriteLine("A diagonal é: " + diagonal.ToString("F2"));
+            retangulo.CalcArea();
+            retangulo.CalcPerimetro();
+            retangulo.CalcDiagonal();
         }
     }
 }
---
+++ CalculoRetangulo/Retangulo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculoRetangulo
+{
+    public class Retangulo
+    {
+        public double Altura;
+        public double Largura;
+
+        public void CalcArea()
+        {
+            // Area A = b * h
+            double area = Altura * Largura;
+            Console.WriteLine($"A área do retângulo é:" + area.ToString("F2"));
+        }
+
+        public void CalcPerimetro()
+        {
+            // Perímmetro P = 2 (b+h)
+            double perimetro = 2 * (Altura + Largura);
+            Console.WriteLine("O perímetro é: " + perimetro.ToString("F2"));
+        }
+
+        public void CalcDiagonal()
+        {
+            // Diagonal a² = b² + c²
+
+            double diagonal = Math.Sqrt((Altura * Altura) + (Largura * Largura));
+            Console.WriteLine("A diagonal é: " + diagonal.ToString("F2"));
+        }
+    }
+}
